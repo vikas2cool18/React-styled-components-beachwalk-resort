f9e399678dc372bd6bd52676da815bb53cbfc61c
Vikas Bharti <vbharti2@whipsystems.com>
2020-04-16 14:00:10 +0530

    first commit

---
+++ src/components/home/Header.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Hero from "../globals/Hero";
+import aboutImg from "../../images/aboutBcg.jpeg";
+import homeImg from "../../images/homeBcg.jpeg";
+import Banner from "../globals/Banner";
+import { PrimaryBtn } from "../globals/Buttons";
+const Header = () => {
+  return (
+    <Hero img={homeImg}>
+      <Banner
+        greeting="welcome to"
+        title="beachwalk resort"
+        text="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi provident eos sapiente deleniti in quisquam."
+      >
+        <PrimaryBtn t="1rem">view details</PrimaryBtn>
+        {/* <PrimaryBtn as="a" href="https://www.google.com">
+          view details
+        </PrimaryBtn> */}
+      </Banner>
+    </Hero>
+  );
+};
+
+export default Header;
---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../components/home/Header";
+import About from "../components/home/About";
+import Rooms from "../components/home/Rooms";
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <About />
+      <Rooms />
+    </>
+  );
+};
+
+export default Home;
